1fd577c8bb1a567c9134b17b41911e718e465113
george <george089@revature.net>
2022-04-05 14:17:21 -0700

    Add POST /users endpoint for creating user

--- src/main/java/com/revature/main/controller/UserController.java
+++ src/main/java/com/revature/main/controller/UserController.java
@@ -3,6 +3,7 @@ package com.revature.main.controller;
 import com.revature.main.model.User;
 import com.revature.main.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +19,10 @@ public class UserController {
     @RequestMapping(value="/users", method= RequestMethod.GET)
     public List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @RequestMapping(value="/users", method=RequestMethod.POST)
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
     }
 }
